d24a3bb8e77b0398ae606e648050b192674bdf05
Kaviid <kaveeshadias27@gmail.com>
2026-09-04 22:36:29 +0530

    Dilatting image and simple not abt it

--- basicFuncs.py
+++ basicFuncs.py
@@ -15,4 +15,21 @@ cv.imshow("Blured Image", blur)
 cany = cv.Canny(img, 175, 50)
 cv.imshow("Canny Edges", cany)
 
+#4. Dilatting image
+dilated = cv.dilate(cany, (7,7), iterations=3)
+cv.imshow("Dilated", dilated)
+
+#  📝 Dilation — Simple Notes
+
+#   cv.dilate() → makes the white/edge regions thicker.
+#   Kernel (7,7) → uses a 7×7 area to expand the pixels.
+#   iterations=3 → dilation is repeated 3 times, so the effect is stronger.
+#   It is mainly used to connect broken edges and make edges more visible.
+#   It can help before finding contours/shapes.
+
+#   Easy idea
+#   Canny → thin edges → Dilation → thicker/connected edges🔗
+#   Remember: Bigger kernel or more iterations = more dilation.
+
+
 cv.waitKey(0)
