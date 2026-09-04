ff567a65babb52ccd348fa47e9ccfc83fb372dec
Kaviid <kaveeshadias27@gmail.com>
2026-09-04 22:27:40 +0530

    Create Canny Edges

--- basicFuncs.py
+++ basicFuncs.py
@@ -11,4 +11,8 @@ cv.imshow("Gray color", gray)
 blur = cv.GaussianBlur(img, (7,7), cv.BORDER_DEFAULT)
 cv.imshow("Blured Image", blur)
 
+#3. Create Edge Cascade
+cany = cv.Canny(img, 175, 50)
+cv.imshow("Canny Edges", cany)
+
 cv.waitKey(0)
